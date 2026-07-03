7e1dcdb8f0b34e247fe79cd7a9cdbac41e19d177
iki1uc <entertrainer@web.de>
2026-07-04 00:19:09 +0200

    Update koOparator.js

--- koOparator.js
+++ koOparator.js
@@ -1,20 +1,19 @@
-import { KOOP } from "./koop.js";
-import { NOOP } from "./noop.js";
-import { POOK } from "./pook.js";
+import { KOOP } from "../KOOP/koop.js";
+import { NOOP } from "../NOOP/noop.js";
+import { POOK } from "../POOK/pook.js";
 
 export function koOparator(respo){
   if(respo === "RUN8" || respo === "TMP" || respo === "3hit90"){
-    return KOOP;   // aktives Verhalten
+    return KOOP;
   }
 
   if(respo === "dir" || respo === "WpiR" || respo === "MXU"){
-    return NOOP;   // enthält sich
+    return NOOP;
   }
 
   if(respo === "CLONE" || respo === "FAIL" || respo === "9vec3tor"){
-    return POOK;   // narrativ
+    return POOK;
   }
 
-  return NOOP; // Default: Enthaltung
+  return NOOP;
 }
-
